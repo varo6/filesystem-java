baf2cfa52f69997e968915ab634e04b742f376ea
varo6 <103251919+varo6@users.noreply.github.com>
2024-12-03 14:42:28 +0100

    Add main.java structure

--- src/main/java/com/filetransfer/Main.java
+++ src/main/java/com/filetransfer/Main.java
@@ -1,8 +1,27 @@
 public class Main {
 
-    public Main() {}
-
     public static void main(String[] args) {
-        System.out.println("Simple filesystem app");
+        if (args.length == 0) {
+            System.out.println(
+                "Por favor, proporciona un argumento: --client o --server"
+            );
+            return;
+        }
+
+        switch (args[0]) {
+            case "--client":
+            case "-c":
+                //iniciar cliente
+                break;
+            case "--server":
+            case "-s":
+                //Iniciamos servidor
+                break;
+            default:
+                System.out.println(
+                    "Argumento no reconocido. Usa --client o --server"
+                );
+                break;
+        }
     }
 }
