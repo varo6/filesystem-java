fdea207462db891509e01b29f32250b76fa51d04
André Yermak Naumenko <asyermak@gmail.com>
2025-01-14 23:36:12 +0100

    Revert "scp upload"
    This reverts commit 2819ba0f06b13c9c5d88b36c7ece3642c8a7f028.

--- src/main/java/com/filetransfer/client/ClientContextHandler.java
+++ src/main/java/com/filetransfer/client/ClientContextHandler.java
@@ -5,9 +5,6 @@ import com.filetransfer.common.Context;
 import com.filetransfer.common.ContextCommandHandler;
 import com.filetransfer.common.ContextManager;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
@@ -41,11 +38,10 @@ public class ClientContextHandler implements ContextCommandHandler {
             case "scp":
                 if (ClientUtils.validateScpArg(command)) {
                     if (command[1].equals("-u")){
-                        Path path = Paths.get("./FileSystem/storage/" + command[2]);
-                        byte[] fileBytes = Files.readAllBytes(path);
                         commandMessage = new CommandMessage.Builder(CommandMessage.CommandType.FILE_UPLOAD)
-                                .addArg(command[2])
-                                .setPayload(fileBytes)
+                                .addArg(command[1])
+                                .addArg(command[3])
+                                .setPayload("Aqui van todos los bytes del archivo a transmitir, la informacion util".getBytes())
                                 .build();
                         break;
                     } else if (command[1].equals("-d")){
--- src/main/java/com/filetransfer/common/Utils.java
+++ src/main/java/com/filetransfer/common/Utils.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.file.Path;
 
 public class Utils {
 
@@ -42,4 +41,5 @@ public class Utils {
         }
         return publicIP;
     }
+
 }
